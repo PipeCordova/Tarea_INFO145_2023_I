6a1d279d9d8663290ce49a0179aa22b16ef5d568
Felipe Córdova <85969736+PipeCordova@users.noreply.github.com>
2023-06-02 23:42:03 -0400

    falta T(n) para encontrarFormasPosibles(...)

--- Subiendo Escalera/Fuerza Bruta/solucionFuerzaBruta.cpp
+++ Subiendo Escalera/Fuerza Bruta/solucionFuerzaBruta.cpp
@@ -11,70 +11,9 @@
 
 using namespace std;
 
+// Declarando la funcion utilizada
+vector<vector<int>> encontrarFormasPosibles(int n, int p, vector<int>& escalones_rotos);
 
-/* En esta funcion se usan varios metodos de vectores. Consideremos que v es un vector entonces...
-    v.push_back(...) --> Agregamos un nuevo elemento al final de v
-    v.empty() --> Para verificar si v esta sin elementos o no.
-    v.pop_back() --> Para eliminar el ultimo elemento de v
-    v.back() --> Para acceder al ultimo elemento de v
-    v.erase(...) --> para eliminar uno o varios elementos de v
-    v.begin() --> Iterador que apunta a la primera posicion de v
-*/
-
-vector<vector<int>> encontrarFormasPosibles(int n, int p, vector<int>& escalones_rotos) {
-    vector<vector<int>> formas; // Esta matriz retornara todos los posibles caminos 
-    
-    vector<int> forma_inicial;
-    
-    vector<vector<int>> formas_actuales;
-    formas_actuales.push_back(forma_inicial);
-    
-    while(!formas_actuales.empty()){
-        vector<int> forma_actual = formas_actuales.back();
-        formas_actuales.pop_back();
-        
-        int pos_actual;
-        if(forma_actual.empty()){
-            pos_actual = 0;
-        }else{
-            pos_actual = forma_actual.back();
-        }
-        
-        if(pos_actual == n){
-            if(!forma_actual.empty() && forma_actual[0] == 0){
-                forma_actual.erase(forma_actual.begin()); // Eliminar el 0 al comienzo
-            }
-            formas.push_back(forma_actual);
-            continue; /* Si estas condiciones se cumplen se pasa a la sgte iteracion. Se salta el
-            codigo faltante */
-        }
-        
-        // La condición i <= log(n) / log(p) garantiza que p^k <= n --> Esto dice el enunciado
-
-        for(int i = 0; i <= int(log(n) / log(p)); i++){
-            int siguiente_escalon = pos_actual + pow(p, i);
-            
-            bool escalon_roto = false;
-            for(int escalon : escalones_rotos){
-                if(siguiente_escalon == escalon){
-                    escalon_roto = true;
-                    break;
-                }
-            }
-            
-            if(escalon_roto == true || siguiente_escalon > n){
-                continue; /* Si estas condiciones se cumplen se pasa a la sgte iteracion. Se salta el
-            codigo faltante */
-            }
-            
-            vector<int> nueva_forma = forma_actual;
-            nueva_forma.push_back(siguiente_escalon);
-            formas_actuales.push_back(nueva_forma);
-        }
-    }
-    
-    return formas;
-}
 
 int main(int argc, char* argv[]){
     if(argc != 4 || atoi(argv[3]) >= atoi(argv[1])){
@@ -155,3 +94,67 @@ int main(int argc, char* argv[]){
     return EXIT_SUCCESS;
 }
 
+
+/* En esta funcion se usan varios metodos de vectores. Consideremos que v es un vector entonces...
+    v.push_back(...) --> Agregamos un nuevo elemento al final de v
+    v.empty() --> Para verificar si v esta sin elementos o no.
+    v.pop_back() --> Para eliminar el ultimo elemento de v
+    v.back() --> Para acceder al ultimo elemento de v
+    v.erase(...) --> para eliminar uno o varios elementos de v
+    v.begin() --> Iterador que apunta a la primera posicion de v
+*/
+
+vector<vector<int>> encontrarFormasPosibles(int n, int p, vector<int>& escalones_rotos) {
+    vector<vector<int>> formas; // Esta matriz retornara todos los posibles caminos 
+    
+    vector<int> forma_inicial;
+    
+    vector<vector<int>> formas_actuales;
+    formas_actuales.push_back(forma_inicial);
+    
+    while(!formas_actuales.empty()){
+        vector<int> forma_actual = formas_actuales.back();
+        formas_actuales.pop_back();
+        
+        int pos_actual;
+        if(forma_actual.empty()){
+            pos_actual = 0;
+        }else{
+            pos_actual = forma_actual.back();
+        }
+        
+        if(pos_actual == n){
+            if(!forma_actual.empty() && forma_actual[0] == 0){
+                forma_actual.erase(forma_actual.begin()); // Eliminar el 0 al comienzo
+            }
+            formas.push_back(forma_actual);
+            continue; /* Si estas condiciones se cumplen se pasa a la sgte iteracion. Se salta el
+            codigo faltante */
+        }
+        
+        // La condición i <= log(n) / log(p) garantiza que p^k <= n --> Esto dice el enunciado
+
+        for(int i = 0; i <= int(log(n) / log(p)); i++){
+            int siguiente_escalon = pos_actual + pow(p, i);
+            
+            bool escalon_roto = false;
+            for(int escalon : escalones_rotos){
+                if(siguiente_escalon == escalon){
+                    escalon_roto = true;
+                    break;
+                }
+            }
+            
+            if(escalon_roto == true || siguiente_escalon > n){
+                continue; /* Si estas condiciones se cumplen se pasa a la sgte iteracion. Se salta el
+            codigo faltante */
+            }
+            
+            vector<int> nueva_forma = forma_actual;
+            nueva_forma.push_back(siguiente_escalon);
+            formas_actuales.push_back(nueva_forma);
+        }
+    }
+    
+    return formas;
+}
