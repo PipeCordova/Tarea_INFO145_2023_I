c6b3af3ab2ed58c3a8e96f168f46da442dedef02
Felipe Córdova <85969736+PipeCordova@users.noreply.github.com>
2023-06-02 22:16:03 -0400

    actualizando

--- Ruta en Grafos/solucionGrafos.cpp
+++ Ruta en Grafos/solucionGrafos.cpp
@@ -1,20 +1,23 @@
 /* Integrantes:
-	- Felipe Córdova
-	- Sebastián Montecinos
+	- Felipe Cordova
+	- Sebastian Montecinos
 */
 #include <iostream>
 using namespace std;
 
 int main(int argc, char* argv[]){
-    if (argc != 4) {
-        cout << "¡¡ERROR!! Tiene que compilar ./prog3 n k m" << endl;
+    if(argc != 4 || atoi(argv[2]) >= atoi(argv[1])){
+        if(argc != 4){
+            cout << "¡¡ERROR!! Tiene que compilar ./prog1 n k m" << endl;
+        }else{ // Obligamos que k < n --> Esto dice el enunciado.
+            cout << "¡¡Debe cumplirse que k < n!!" << endl;
+        }
         return EXIT_FAILURE;
     }
     cout << "¡¡Bienvenido Al Problema Ruta En Grafos Mediante Algoritmos Greedy!!" << endl;
     int n = atoi(argv[1]);
     int k = atoi(argv[2]);
     int m = atoi(argv[3]);
-    cout << "Ingreso n = " << n << " y k = " << k << " y m = " << m << endl;
-    cout << "¡Fin Solución!" << endl;
+    cout << "¡¡Fin Solucion!!" << endl;
     return EXIT_SUCCESS;
 }
