7385abb7455f6b63cc907bc2d9a4eee8a57ffc20
Felipe Córdova <85969736+PipeCordova@users.noreply.github.com>
2023-06-02 22:17:24 -0400

    actualizando

--- Ruta en Grafos/solucionGrafos.cpp
+++ Ruta en Grafos/solucionGrafos.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char* argv[]){
     if(argc != 4 || atoi(argv[2]) >= atoi(argv[1])){
         if(argc != 4){
-            cout << "¡¡ERROR!! Tiene que compilar ./prog1 n k m" << endl;
+            cout << "¡¡ERROR!! Tiene que compilar ./prog3 n k m" << endl;
         }else{ // Obligamos que k < n --> Esto dice el enunciado.
             cout << "¡¡Debe cumplirse que k < n!!" << endl;
         }
