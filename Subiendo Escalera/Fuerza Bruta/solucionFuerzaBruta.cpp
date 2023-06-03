1507d94bf89e0fb4f82ae286f27c490e368bb608
Felipe Córdova <85969736+PipeCordova@users.noreply.github.com>
2023-06-03 14:40:02 -0400

    mejorando el codigo

--- Subiendo Escalera/Fuerza Bruta/solucionFuerzaBruta.cpp
+++ Subiendo Escalera/Fuerza Bruta/solucionFuerzaBruta.cpp
@@ -2,98 +2,64 @@
 	- Felipe Cordova
 	- Sebastian Montecinos
 */
-
 #include <iostream> 
 #include <vector> // Trabajamos con vectores
 #include <cmath> // Para usar la funcion pow y log
 #include <stdlib.h> // Para los numeros randoms
 #include <algorithm> // Para utilizar el metodo sort
-
 using namespace std;
 
-// Declarando la funcion utilizada
+// Declarando la funciones utilizadas
 vector<vector<int>> encontrarFormasPosibles(int n, int p, vector<int>& escalones_rotos);
+vector<int> generarIndicesAleatorios(int n, int r);
+void imprimirFormasPosibles(const vector<vector<int>>& formas);
+void imprimirEscalonesRotos(const vector<int>& escalonesRotos);
 
-
+/* Programa principal. Se extrae n,p,r desde los argumentos realizando las verificaciones 
+correspondientes. Luego se crea un vector el cual sera el retorno de la funcion que genera 
+los indices con r escalones rotos. Finalmente se crea una matriz de vectores para almacenar los
+posibles caminos que tiene nuestro heroe Super Mario para llegar al final de la escalera sin pisar 
+los escalones rotos. Como se menciona en el README, recomendamos comentar las funciones imprimir si 
+va a ejecutar con entradas muy grandes. */
 int main(int argc, char* argv[]){
     if(argc != 4 || atoi(argv[3]) >= atoi(argv[1])){
         if(argc != 4){
             cout << "¡¡ERROR!! Tiene que compilar ./prog1 n p r" << endl;
-        }else{ // Obligamos que r < n --> Esto dice el enunciado. Se pueden pedir mas condiciones... 
+        }else{// Obligamos que r < n --> Esto dice el enunciado. Se pueden pedir mas condiciones... 
             cout << "¡¡Debe cumplirse que r < n!!" << endl;
         }
-        return EXIT_FAILURE;
+        return EXIT_FAILURE; // --> Se termina el programa de manera inesperada.
     }
-
     cout << "¡¡Bienvenido Al Problema Subiendo La Escalera Hecho Por Fuerza Bruta!!" << endl;
-
     int n = atoi(argv[1]); // Numero de escalones
-    int p = atoi(argv[2]); // Potencia de salto
+    int p = atoi(argv[2]); // Base de la otencia de salto
     int r = atoi(argv[3]); // Numero de escalones rotos
-
-    // vector para almacenar los indices de los escalones rotos
-    vector<int> escalones_rotos(r);
-
-    // Inicializar la semilla con el tiempo actual, asi tenemos randoms distintos en cada ejecucion
-    srand(time(0));
-
-    // Aqui se crean los indices aleatorios de los escalones rotos. Desde 1 hasta n-1
-    int k_aleatorio;
-
-    for(int i = 0; i < r; i++){ // O(r)
-        // rango de k_aleatorio es 1..n-1 --> El ultimo escalon no puede estar roto
-        k_aleatorio = 1 + rand() % (n - 1);
-        // verificando que siempre se agregen indices distintos. El costo de este while en el peor caso es O(r-1)
-        while(find(escalones_rotos.begin(), escalones_rotos.begin() + i, k_aleatorio) != escalones_rotos.begin() + i) {
-            k_aleatorio = 1 + rand() % (n - 1);
-        }
-        
-        escalones_rotos[i] = k_aleatorio;
-    } // --> Costo total de este ciclo anidado es O(r(r-1)) <--> O(r²)
-
-
-    // Inicio posible comentario
-
-    // Mostrar el vector de escalones_rotos
-    cout << "Los escalones rotos son: ";
-    for(int indice : escalones_rotos) 
-        cout << indice << " ";
-    cout << "\n¡¡Tenga cuidado de pisarlos!!" << endl;
-
-    // Fin posible comentario
-
-
-    // Matriz de vectores el cual retorna los caminos posibles para Super Mario
+    vector<int> escalones_rotos = generarIndicesAleatorios(n, r);
+    imprimirEscalonesRotos(escalones_rotos); 
     vector<vector<int>> formas = encontrarFormasPosibles(n, p, escalones_rotos);
-    
-
-    // Imprimir el número total de formas posibles
-    cout << "Numero total de formas posibles: " << formas.size() << endl;
-
-    // Inicio posible comentario
-
-    // Si hay 0 formas posibles, terminamos el programa, asi evitamos perder tiempo imprimiendo nada
-    if(formas.size() == 0){
-        cout << "No puede subir :c" << endl;
-    }else{
-        // Imprimir las formas posible
-        for(unsigned int i = 0; i < formas.size(); i++){
-            cout << "Forma " << i + 1 << ": ";
-            for(unsigned int j = 0; j < formas[i].size(); j++){
-                cout << formas[i][j] << " ";
-            }
-            cout << endl;
-        }
-    } 
-    /* No consideraremos el costo de imprimir, ya que para entradas grandes el costo asintotico 
-    se inflaria de manera innecesaria */
-
-    // Fin posible comentario
-
+    imprimirFormasPosibles(formas);
     cout << "¡¡Fin Fuerza Bruta!!" << endl;
     return EXIT_SUCCESS;
 }
 
+/* Funcion que genera los indices aleatorios para los escalones rotos. Se crea un vector de largo r
+y se genera una semilla con el tiempo actual, asi tenemos randoms distintos en cada ejecucion.
+Luego simplemente se generan los r numeros aleatorios en el rango 1..n-1, validando que no se repitan. 
+Finalmente se agregan al vector en su posicion i respectiva. */
+vector<int> generarIndicesAleatorios(int n, int r) {
+    vector<int> escalones_rotos(r);
+    srand(time(0));
+    int k_aleatorio;
+    for(int i = 0; i < r; i++){ // O(r)
+        k_aleatorio = 1 + rand() % (n - 1);
+        // El costo de este while en el peor caso es O(r-1)
+        while(find(escalones_rotos.begin(), escalones_rotos.begin() + i, k_aleatorio) != escalones_rotos.begin() + i){
+            k_aleatorio = 1 + rand() % (n - 1);
+        }
+        escalones_rotos[i] = k_aleatorio;
+    } // --> Costo total de este ciclo anidado es O(r(r-1)) <--> O(r²)
+    return escalones_rotos;
+}
 
 /* En esta funcion se usan varios metodos de vectores. Consideremos que v es un vector entonces...
     v.push_back(...) --> Agregamos un nuevo elemento al final de v
@@ -103,7 +69,6 @@ int main(int argc, char* argv[]){
     v.erase(...) --> para eliminar uno o varios elementos de v
     v.begin() --> Iterador que apunta a la primera posicion de v
 */
-
 vector<vector<int>> encontrarFormasPosibles(int n, int p, vector<int>& escalones_rotos) {
     vector<vector<int>> formas; // Esta matriz retornara todos los posibles caminos 
     
@@ -131,9 +96,7 @@ vector<vector<int>> encontrarFormasPosibles(int n, int p, vector<int>& escalones
             continue; /* Si estas condiciones se cumplen se pasa a la sgte iteracion. Se salta el
             codigo faltante */
         }
-        
         // La condición i <= log(n) / log(p) garantiza que p^k <= n --> Esto dice el enunciado
-
         for(int i = 0; i <= int(log(n) / log(p)); i++){
             int siguiente_escalon = pos_actual + pow(p, i);
             
@@ -144,12 +107,10 @@ vector<vector<int>> encontrarFormasPosibles(int n, int p, vector<int>& escalones
                     break;
                 }
             }
-            
             if(escalon_roto == true || siguiente_escalon > n){
                 continue; /* Si estas condiciones se cumplen se pasa a la sgte iteracion. Se salta el
             codigo faltante */
             }
-            
             vector<int> nueva_forma = forma_actual;
             nueva_forma.push_back(siguiente_escalon);
             formas_actuales.push_back(nueva_forma);
@@ -157,4 +118,32 @@ vector<vector<int>> encontrarFormasPosibles(int n, int p, vector<int>& escalones
     }
     
     return formas;
+}
+
+/* En las siguientes funciones de imprimir, no consideraremos el costo asintotico, ya que para 
+entradas grandes el costo asintotico se inflaria de manera innecesaria. */
+
+void imprimirEscalonesRotos(const vector<int>& escalonesRotos) {
+    cout << "Los escalones rotos son: ";
+    for(int indice : escalonesRotos) {
+        cout << indice << " ";
+    }
+    cout << "\n¡¡Tenga cuidado de pisarlos!!" << endl;
+    return;
+}
+
+void imprimirFormasPosibles(const vector<vector<int>>& formas) {
+    cout << "Numero total de formas posibles: " << formas.size() << endl;
+    if(formas.size() == 0){
+        cout << "No puede subir :c" << endl;
+    }else{
+        for(unsigned int i = 0; i < formas.size(); i++){
+            cout << "Forma " << i + 1 << ": ";
+            for(unsigned int j = 0; j < formas[i].size(); j++){
+                cout << formas[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return;
 }
