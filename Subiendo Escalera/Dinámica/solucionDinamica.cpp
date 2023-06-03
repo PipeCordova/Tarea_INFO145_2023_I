295653fbe33f49f31d0e0ab0f3f3da5b98673119
Felipe Córdova <85969736+PipeCordova@users.noreply.github.com>
2023-06-02 22:04:41 -0400

    actualizando

--- Subiendo Escalera/Dinámica/solucionDinamica.cpp
+++ Subiendo Escalera/Dinámica/solucionDinamica.cpp
@@ -1,19 +1,23 @@
 /* Integrantes:
-	- Felipe Córdova
-	- Sebastián Montecinos
+	- Felipe Cordova
+	- Sebastian Montecinos
 */
 #include <iostream>
 using namespace std;
 
 int main(int argc, char* argv[]){
-    if (argc != 3) {
-        cout << "¡¡ERROR!! Tiene que compilar ./prog2 r p" << endl;
+    if(argc != 4 || atoi(argv[3]) >= atoi(argv[1])){
+        if(argc != 4){
+            cout << "¡¡ERROR!! Tiene que compilar ./prog2 n p r" << endl;
+        }else{ // Obligamos que r < n --> Esto dice el enunciado. Se pueden pedir mas condiciones... 
+            cout << "¡¡Debe cumplirse que r < n!!" << endl;
+        }
         return EXIT_FAILURE;
     }
-    cout << "¡¡Bienvenido Al Problema Subiendo La Escalera Hecho Por Programación Dinámica!!" << endl;
-    int r = atoi(argv[1]);
-    int p = atoi(argv[2]);
-    cout << "Ingreso r = " << r << " y p = " << p << endl;
-    cout << "¡¡Fin Solución Dinámica!!" << endl;
+    cout << "¡¡Bienvenido Al Problema Subiendo La Escalera Hecho Por Programacion Dinamica!!" << endl;
+    int n = atoi(argv[1]); // Numero de escalones
+    int p = atoi(argv[2]); // Potencia de salto
+    int r = atoi(argv[3]); // Numero de escalones rotos
+    cout << "¡¡Fin Solucion Dinamica!!" << endl;
     return EXIT_SUCCESS;
 }
