ed468ea2b8805962b1d156656564fe9843ab43cb
SebaMonte07 <seb.monte17@gmail.com>
2023-06-10 02:44:02 -0400

    Escalera PD

--- Subiendo Escalera/Fuerza Bruta/formaSeba.cpp
+++ Subiendo Escalera/Fuerza Bruta/formaSeba.cpp
@@ -3,6 +3,8 @@
 #include <algorithm> // Para utilizar el metodo find
 #include <set> // Para usar set y su función insert()
 #include <ctime> // Para cambiar la semilla y medir tiempo
+#define FB 1 // 1 para mostrar la solución por Fuerza Bruta, 0 para no mostarla
+#define PD 1 // 1 para mostrar la solución por Programación Dinámica, 0 para no mostarla
 using namespace std; // Para usar cout y no std::cout
 
 vector<int> creaPotencias(int p, int n);
@@ -10,6 +12,7 @@ void imprimeVector(vector<int> v);
 void generaRotos(int cant, int n, vector<int>& rotos);
 int formasPSC(int n, int k, vector<int>& B, vector<int>& rotos);
 int formas(int n, int k, vector<int>& B, vector<int>& rotos, vector<int>& formaActual, vector<int>& ultimaForma);
+long long formasPD(int n, int k, vector<int>& B, vector<int>& rotos);
 
 
 int main(int argc, char* argv[]){
@@ -24,7 +27,7 @@ int main(int argc, char* argv[]){
 
     // Para cambiar los numeros en cada ejecucion
     srand(time(0));
-    
+
     // Declaración de Variables
     int n = atoi(argv[1]); // Numero de escalones
     int p = atoi(argv[2]); // Base de la potencia de salto
@@ -35,8 +38,12 @@ int main(int argc, char* argv[]){
     generaRotos(r, n, rotos);
     vector<int> formaActual;
     vector<int> ultimaForma;
+    vector<int> Aux(n-1);
 
-    cout << "=====================================" << endl;
+
+    cout << "\n======================" << endl;
+    cout << "|| DATOS DE ENTRADA ||" << endl;
+    cout << "======================" << endl;
     cout << "n (escalones)\t= " << n << endl;
     cout << "p (base pot)\t= " << p << endl;
     cout << "Potencias\t= ";
@@ -44,18 +51,36 @@ int main(int argc, char* argv[]){
     cout << "r (rotos)\t= " << r << endl;
     cout << "Rotos\t\t= ";
     imprimeVector(rotos);
-    cout << "=====================================" << endl;
+    if(FB){
+        cout << "\n===============================" << endl;
+        cout << "|| ALGORITMO DE FUERZA BRUTA ||" << endl;
+        cout << "===============================" << endl;
+        //cout << "Formas posibles PSC = " << formasPSC(n, k, potencias, rotos) << endl;
+        clock_t t1 = clock();
+        int f1 = formas(n,k,potencias,rotos,formaActual, ultimaForma);
+        clock_t t2 = clock();
+        double secs1 = static_cast<double>(t2-t1)/CLOCKS_PER_SEC;
+        cout << "Formas posibles\t\t= " << f1 << endl;
+        cout << "Tiempo de ejecución\t= " << secs1 << " segundos" << endl;
+        // El vector ultima forma queda con los indices al reves, aplicamos reverse para se imprima bien la solución
+        cout << "Forma mas corta\t\t= ";
+        reverse(ultimaForma.begin(),ultimaForma.end());
+        imprimeVector(ultimaForma);
+    }
 
-    //cout << "Formas posibles PSC = " << formasPSC(n, k, potencias, rotos) << endl;
-    int f = formas(n,k,potencias,rotos,formaActual, ultimaForma);
-    cout << "Formas posibles = " << f << endl;
-    // El vector ultima forma queda con los indices al reves, aplicamos reverse para se imprima bien la solución
-    cout << "Forma mas corta = ";
-    reverse(ultimaForma.begin(),ultimaForma.end());
-    imprimeVector(ultimaForma);
-    cout << "=====================================" << endl;
+    if(PD){
+        cout << "\n=============================================" << endl;
+        cout << "|| ALGORITMO DE PROGRAMACION DINAMICA (PD) ||" << endl;
+        cout << "=============================================" << endl;
 
-    
+        clock_t t3 = clock();
+        long long f2 = formasPD(n, k, potencias, rotos) ;
+        clock_t t4 = clock();
+        double secs2 = static_cast<double>(t4-t3)/CLOCKS_PER_SEC;
+        cout << "Formas posibles\t\t= " << f2 << endl;
+        cout << "Tiempo de ejecución\t= " << secs2 << " segundos" << endl;
+    }
+    return EXIT_SUCCESS;
 }
 
 int formas(int n, int k, vector<int>& B, vector<int>& rotos, vector<int>& formaActual, vector<int>& ultimaForma) {
@@ -96,6 +121,21 @@ int formasPSC(int n, int k, vector<int>& B, vector<int>& rotos) {
     }
     
     return total;
+}
+
+long long formasPD(int n, int k, vector<int>& B, vector<int>& rotos) {
+    vector<long long> aux(n + 1, 0);
+    aux[0] = 1; // Caso base: hay una forma de subir una escalera de longitud 0
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < k; j++) {
+            if (i - B[j] >= 0 && find(rotos.begin(), rotos.end(), i) == rotos.end()) {
+                aux[i] += aux[i - B[j]];
+            }
+        }
+    }
+    
+    return aux[n];
 }
 
 vector<int> creaPotencias(int p, int n) {
